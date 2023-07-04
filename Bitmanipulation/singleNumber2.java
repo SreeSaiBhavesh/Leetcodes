d6c36f55e9575cbad69c6ff8fde3a9feb1f6d7fb
VSSB <78292660+SreeSaiBhavesh@users.noreply.github.com>
2023-07-04 09:19:36 +0530

    Added singlenumber2

---
+++ Bitmanipulation/singleNumber2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int e : nums) {
+            ones = ((ones ^ e) & (~ twos));  
+            twos = ((twos ^ e) & (~ ones)); 
+        }
+        return ones;  
+    }
+}
